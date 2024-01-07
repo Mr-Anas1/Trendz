ff1694b00912158b20977e90107eae38f33300f5
Mohamed Anas <141704241+Mr-Anas1@users.noreply.github.com>
2024-01-07 15:41:55 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+var sidenavbar = document.querySelector(".side-navbar");
+
+function shownavbar(){
+    sidenavbar.style.left="0";
+}
+
+function hidenavbar(){
+    sidenavbar.style.left="-60%";
+}
+
